fd0fb85744b528387b046328f928bf538f0fecc3
PerfectPoipole <perfectpoipole803@gmail.com>
2023-07-25 14:43:52 -0400

    lool

--- data/mods/gen1/pokedex.ts
+++ data/mods/gen1/pokedex.ts
@@ -609,4 +609,8 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
 	},
+	celebi: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
+	},
 };
