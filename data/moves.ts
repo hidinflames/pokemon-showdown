2f144f3fdd001a5f9f8df1f664d1a2593ce73f1f
PerfectPoipole <perfectpoipole803@gmail.com>
2023-12-16 13:59:44 -0500

    plz

--- data/moves.ts
+++ data/moves.ts
@@ -19980,7 +19980,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		basePower: 120,
 		category: "Special",
 		name: "Tera Starstorm",
-		pp: 5,
+		pp: 10,
 		priority: 0,
 		flags: {protect: 1, mirror: 1, noassist: 1, failcopycat: 1, failmimic: 1},
 		onModifyType(move, pokemon) {
